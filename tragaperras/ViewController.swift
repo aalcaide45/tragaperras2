a54e731e9560b86ee61469113f948731fc29af88
ALFONSO ALCAIDE <alfonsoalcaide@mac-mini-de-alfonso.local>
2018-08-13 01:35:55 +0200

    Funcionando a falta de poner las frutas en sus respectivos displays. Falta que analice el premio y qué anuncie el premio ganado. El bolsillo funciona. NO GUARDA LOS DATOS.

--- tragaperras/ViewController.swift
+++ tragaperras/ViewController.swift
@@ -30,19 +30,60 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayDerecho: UILabel!
     
     
-    
     @IBOutlet weak var miDinero: UILabel!
     
     
     
+    var pantallaIzquierdo : Int = 1
+    var pantallaCentro : Int = 1
+    var pantallaDerecha : Int = 1
+    
+    
+    var bolsilloInt : Int = 0
+    
+    let limiteIconos : [String] = ["0", "1", "2", "3", "4", "5", "6"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let bolsillo = miDinero.text!
+        bolsilloInt = Int (bolsillo)!
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func pulsador(_ sender: UIButton) {
+        tirada()
+        bajaDinero()
     }
     
+    func tirada() {
+        let caras : UInt32 = UInt32 (limiteIconos.count)
+        
+        pantallaIzquierdo = Int(arc4random_uniform(caras))
+        pantallaCentro = Int(arc4random_uniform(caras))
+        pantallaDerecha = Int(arc4random_uniform(caras))
+        
+        displayIzquierdo.text = String (pantallaIzquierdo)
+        displayCentro.text = String (pantallaCentro)
+        displayDerecho.text = String (pantallaDerecha)
+       
+        print("Pantalla Izquierda \(pantallaIzquierdo)")
+        print("Pantalla Central \(pantallaCentro)")
+        print("Pantalla Derecha \(pantallaDerecha)")
+    }
+    
+    func bajaDinero() {
+        
+        bolsilloInt -= 1
+        miDinero.text = String(bolsilloInt)
+        
+        print(bolsilloInt)
+    }
+    
+    func compruebaResultado() {
+        
+        
+    }
 }
 
